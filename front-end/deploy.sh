3403e36bbd38ed6352b9d0db3f3a135d024584b1
DavyBellens <r0882857@ucll.be>
2024-03-14 19:19:37 +0100

    removed node server.js & line becuase it is unnecessary

--- front-end/deploy.sh
+++ front-end/deploy.sh
@@ -6,4 +6,3 @@ cp -r ./public/ standalone/
 sed -i 's/const currentPort = parseInt(process.env.PORT, 10) || 3000/const currentPort = 27012/' standalone/server.js
 sed -i "s/const hostname = process.env.HOSTNAME || '0.0.0.0'/const hostname = '0.0.0.0'/" standalone/server.js
 cd standalone
-node server.js &
