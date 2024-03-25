8240b2e0066d5ef560c7f5b4e819ee549fbd6571
DavyBellens <r0882857@ucll.be>
2024-03-25 12:32:34 +0100

    custom error messages

--- front-end/components/profiles/socials/Socials.tsx
+++ front-end/components/profiles/socials/Socials.tsx
@@ -13,10 +13,36 @@ const SocialsForm: React.FC<Props> = ({ socials, callBack, setStage }: Props) =>
   const [snapchat, setSnapchat] = useState<string>(socials ? socials[2] : "");
   const [messenger, setMessenger] = useState<string>(socials ? socials[3] : "");
   const [phoneNumber, setPhoneNumber] = useState<string>(socials ? socials[4] : "");
+  const [instagramError, setInstagramError] = useState<string>("");
+  const [facebookError, setFacebookError] = useState<string>("");
+  const [snapchatError, setSnapchatError] = useState<string>("");
+  const [messengerError, setMessengerError] = useState<string>("");
+  const [phoneNumberError, setPhoneNumberError] = useState<string>("");
   const [error, setError] = useState<string>("");
   const validate = () => {
-    const socialsList = [instagram, facebook, snapchat, messenger, phoneNumber];
     let isValid = true;
+    if (instagram && instagram.length > 30) {
+      setInstagramError("Instagram username can't be longer than 30 characters");
+      isValid = false;
+    }
+
+    if (facebook && facebook.length > 50) {
+      setFacebookError("Facebook username can't be longer than 50 characters");
+      isValid = false;
+    }
+    if (snapchat && snapchat.length > 30) {
+      setSnapchatError("Snapchat username can't be longer than 30 characters");
+      isValid = false;
+    }
+    if (messenger && messenger.length > 50) {
+      setMessengerError("Messenger username can't be longer than 50 characters");
+      isValid = false;
+    }
+    if (phoneNumber && phoneNumber.replace(" ", "").length > 12) {
+      setPhoneNumberError("Phone number can't be longer than 12 digits");
+      isValid = false;
+    }
+    const socialsList = [instagram, facebook, snapchat, messenger, phoneNumber];
     const empty = socialsList.filter((s) => s == "" || s == undefined);
     if (empty.length === socialsList.length) {
       setError("At least one is required");
@@ -44,51 +70,66 @@ const SocialsForm: React.FC<Props> = ({ socials, callBack, setStage }: Props) =>
           <br />
           <small>(Only 1 required)</small>
         </h1>
-        <InputField
-          labelStyle={labelStyle}
-          inputStyle={input}
-          callBack={setInstagram}
-          field="Instagram"
-          id="instagramInput"
-          type="text"
-          value={instagram}
-        />
-        <InputField
-          labelStyle={labelStyle}
-          inputStyle={input}
-          callBack={setFacebook}
-          field="Facebook"
-          id="facebookInput"
-          type="text"
-          value={facebook}
-        />
-        <InputField
-          labelStyle={labelStyle}
-          inputStyle={input}
-          callBack={setSnapchat}
-          field="Snapchat"
-          id="snapchatInput"
-          type="text"
-          value={snapchat}
-        />
-        <InputField
-          labelStyle={labelStyle}
-          inputStyle={input}
-          callBack={setMessenger}
-          field="Messenger"
-          id="messengerInput"
-          type="text"
-          value={messenger}
-        />
-        <InputField
-          labelStyle={labelStyle}
-          inputStyle={input}
-          callBack={setPhoneNumber}
-          field="Phone Number"
-          id="phoneNumberInput"
-          type="tel"
-          value={phoneNumber}
-        />
+        <div className="flex flex-col">
+          <InputField
+            labelStyle={labelStyle}
+            inputStyle={input}
+            callBack={setInstagram}
+            field="Instagram"
+            id="instagramInput"
+            type="text"
+            value={instagram}
+          />
+          {instagramError}
+        </div>
+        <div className="flex flex-col">
+          <InputField
+            labelStyle={labelStyle}
+            inputStyle={input}
+            callBack={setFacebook}
+            field="Facebook"
+            id="facebookInput"
+            type="text"
+            value={facebook}
+          />
+          {facebookError}
+        </div>
+        <div className="flex flex-col">
+          <InputField
+            labelStyle={labelStyle}
+            inputStyle={input}
+            callBack={setSnapchat}
+            field="Snapchat"
+            id="snapchatInput"
+            type="text"
+            value={snapchat}
+          />
+          {snapchatError}
+        </div>
+        <div className="flex flex-col">
+          <InputField
+            labelStyle={labelStyle}
+            inputStyle={input}
+            callBack={setMessenger}
+            field="Messenger"
+            id="messengerInput"
+            type="text"
+            value={messenger}
+          />
+          {messengerError}
+        </div>
+        <div className="flex flex-col">
+          <InputField
+            labelStyle={labelStyle}
+            inputStyle={input}
+            callBack={setPhoneNumber}
+            field="Phone Number"
+            id="phoneNumberInput"
+            type="tel"
+            value={phoneNumber}
+          />
+          {phoneNumberError}
+        </div>
         {error && <div>{error}</div>}
       </form>
       <div className="flex justify-between w-full mt-3">
