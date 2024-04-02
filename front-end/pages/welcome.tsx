e59b17479a02294d1bc14b57eb8999306b051512
DavyBellens <r0882857@ucll.be>
2024-04-02 22:19:44 +0200

    welcome page

--- front-end/pages/welcome.tsx
+++ front-end/pages/welcome.tsx
@@ -1,6 +1,6 @@
 import Footer from "@/components/Footer";
 import b from "@/public/images/BioMedica_logo.png";
-import p from "@/public/images/SPRIMUS_PNG.png";
+import p from "@/public/images/primus-logo.jpg";
 import wd from "@/public/images/We're Different Agency LOGO (Wit-Blauw).png";
 import td from "@/public/images/td_logo.png";
 import Head from "next/head";
@@ -18,11 +18,23 @@ const Welcome: React.FC = () => {
         <div className="flex flex-col justify-center align-middle text-center">
           <p className="mt-20 text-4xl font-bold">Welcome to</p>
           <Image src={td} alt="Logo Tinder TD" width={250} height={50} className="m-auto mt-10 mb-10" />
+          <a
+            href="/login"
+            className="bg-white bg-opacity-50 m-auto p-2 rounded-xl font-bold text-3xl text-center w-max"
+          >
+            Continue
+          </a>
           <p className="mt-10 mb-10 text-2xl font-semibold">Made possible by</p>
-          <div className="grid grid-rows-3 justify-center">
+          <div className="grid grid-cols-2 grid-rows-2 justify-center">
             <Image src={b} alt="Logo Biomedica" width={150} height={100} className="m-auto mb-10" />
-            <Image src={p} alt="Logo Primus Scaldiae" width={175} height={100} className="m-5 mb-0" />
-            <Image src={wd} alt="Logo We're Different Agency" width={175} height={100} className="m-auto mt-0" />
+            <Image src={p} alt="Logo Primus Scaldiae" width={200} height={200} className="mt-5" />
+            <Image
+              src={wd}
+              alt="Logo We're Different Agency"
+              width={175}
+              height={100}
+              className="m-auto mt-0 col-span-2"
+            />
           </div>
         </div>
       </main>
