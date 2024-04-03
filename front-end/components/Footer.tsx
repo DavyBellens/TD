5efd7b8135b268875cb7444fa2f54a52d19e24b8
DavyBellens <r0882857@ucll.be>
2024-04-03 14:18:48 +0200

    added link to personal website

--- front-end/components/Footer.tsx
+++ front-end/components/Footer.tsx
@@ -4,7 +4,9 @@ const Footer: React.FC = () => {
   return (
     <footer className="p-5 sm:p-10">
       <div className="flex justify-center">
-        <p className="text-md">&copy; 2024 - Davy Bellens</p>
+        <a href="https://www.davybellens.com" target="_blank">
+          <p className="text-md">&copy; 2024 - Davy Bellens</p>
+        </a>
       </div>
     </footer>
   );
